6a6842ca9a22827da16e2dbcedfdf2525c068c3d
Henning Eggers <henning.eggers@inovex.de>
2025-11-08 00:48:22 +0100

    feat: init function button callbacks

--- src/buttons.c
+++ src/buttons.c
@@ -1,30 +1,29 @@
+#include <stdint.h>
 #include <sys/_types.h>
 #include <zephyr/kernel.h>
 #include <zephyr/drivers/gpio.h>
 #include <zephyr/logging/log.h>
 
 #include "thread_prios.h"
-#include "queue.h"
+#include "leds.h"
 
 LOG_MODULE_REGISTER(BUTTONS);
 
 #define BTN_FORWARD DT_ALIAS(btn_forward)
 #define BTN_BACKWARD DT_ALIAS(btn_backward)
 #define BTN_STOP DT_ALIAS(btn_stop)
-
-#if !DT_NODE_HAS_STATUS(BTN_FORWARD, okay)
-#error "Unsupported board: btn-forward devicetree alias is not defined"
-#endif
-#if !DT_NODE_HAS_STATUS(BTN_BACKWARD, okay)
-#error "Unsupported board: btn-backward devicetree alias is not defined"
-#endif
-#if !DT_NODE_HAS_STATUS(BTN_STOP, okay)
-#error "Unsupported board: btn-stop devicetree alias is not defined"
-#endif
+#define BTN_WHITE_LEFT DT_ALIAS(btn_white_left)
+#define BTN_WHITE_RIGHT DT_ALIAS(btn_white_right)
+#define BTN_YELLOW DT_ALIAS(btn_yellow)
+#define BTN_BLACK DT_ALIAS(btn_black)
+#define BTN_GREEN DT_ALIAS(btn_green)
+#define BTN_BLUE DT_ALIAS(btn_blue)
 
 static int btn_init();
-static void btn_init_callback(void);
-static void btn_pressed(const struct device *dev, struct gpio_callback *cb, uint32_t pins);
+static void btn_ctrl_init_callback(void);
+static void btn_func_init_callback(void);
+static void btn_ctrl_pressed(const struct device *dev, struct gpio_callback *cb, uint32_t pins);
+static void btn_func_pressed(const struct device *dev, struct gpio_callback *cb, uint32_t pins);
 
 SYS_INIT(btn_init, APPLICATION, BTN_INIT_PRIO);
 
@@ -33,65 +32,119 @@ struct buttons
 	struct gpio_dt_spec forward;
 	struct gpio_dt_spec backward;
 	struct gpio_dt_spec stop;
+	struct gpio_dt_spec white_left;
+	struct gpio_dt_spec white_right;
+	struct gpio_dt_spec yellow;
+	struct gpio_dt_spec black;
+	struct gpio_dt_spec green;
+	struct gpio_dt_spec blue;
 };
 
 static struct buttons buttons = {
 	.forward = GPIO_DT_SPEC_GET(BTN_FORWARD, gpios),
 	.backward = GPIO_DT_SPEC_GET(BTN_BACKWARD, gpios),
-	.stop = GPIO_DT_SPEC_GET(BTN_STOP, gpios)
+	.stop = GPIO_DT_SPEC_GET(BTN_STOP, gpios),
+	.white_left = GPIO_DT_SPEC_GET(BTN_WHITE_LEFT, gpios),
+	.white_right = GPIO_DT_SPEC_GET(BTN_WHITE_RIGHT, gpios),
+	.yellow = GPIO_DT_SPEC_GET(BTN_YELLOW, gpios),
+	.black = GPIO_DT_SPEC_GET(BTN_BLACK, gpios),
+	.green = GPIO_DT_SPEC_GET(BTN_GREEN, gpios),
+	.blue = GPIO_DT_SPEC_GET(BTN_BLUE, gpios)
 };
 
 static int btn_init()
 {
 	gpio_pin_configure_dt(&buttons.forward, GPIO_INPUT);
-	if (!gpio_is_ready_dt(&buttons.forward)) {
-		LOG_ERR("Error: forward buttons device not ready");
-	}
 	gpio_pin_configure_dt(&buttons.backward, GPIO_INPUT);
-	if (!gpio_is_ready_dt(&buttons.backward)) {
-		LOG_ERR("Error: backward buttons device not ready");
-	}
 	gpio_pin_configure_dt(&buttons.stop, GPIO_INPUT);
-	if (!gpio_is_ready_dt(&buttons.stop)) {
-		LOG_ERR("Error: stop buttons device not ready");
-	}
+	gpio_pin_configure_dt(&buttons.white_left, GPIO_INPUT);
+	gpio_pin_configure_dt(&buttons.white_right, GPIO_INPUT);
+	gpio_pin_configure_dt(&buttons.yellow, GPIO_INPUT);
+	gpio_pin_configure_dt(&buttons.black, GPIO_INPUT);
+	gpio_pin_configure_dt(&buttons.green, GPIO_INPUT);
+	gpio_pin_configure_dt(&buttons.blue, GPIO_INPUT);
 
-	btn_init_callback();
+	btn_ctrl_init_callback();
+	btn_func_init_callback();
+
 	LOG_INF("Buttons initialized");
 	return 0;
 }
 
-static struct gpio_callback buttons_cb_data;
+static struct gpio_callback buttons_ctrl_cb_data;
+static struct gpio_callback buttons_func_cb_data;
 
-static void btn_init_callback(void)
+static void btn_ctrl_init_callback(void)
 {
 	unsigned int pin_mask = BIT(buttons.forward.pin) | BIT(buttons.backward.pin) | BIT(buttons.stop.pin);
 	gpio_pin_interrupt_configure_dt(&buttons.forward, GPIO_INT_EDGE_FALLING);
 	gpio_pin_interrupt_configure_dt(&buttons.backward, GPIO_INT_EDGE_FALLING);
 	gpio_pin_interrupt_configure_dt(&buttons.stop, GPIO_INT_EDGE_FALLING);
-	gpio_init_callback(&buttons_cb_data, btn_pressed, pin_mask);
-	gpio_add_callback(buttons.forward.port, &buttons_cb_data);
-	LOG_INF("Set up callback for buttons");
+	gpio_init_callback(&buttons_ctrl_cb_data, btn_ctrl_pressed, pin_mask);
+	gpio_add_callback(buttons.forward.port, &buttons_ctrl_cb_data);
+	LOG_INF("Set up callback for control buttons");
 }
 
-static void btn_pressed(const struct device *dev, struct gpio_callback *cb, uint32_t pins)
+static void btn_func_init_callback(void)
+{
+	unsigned int pin_mask = 0;
+	pin_mask |= BIT(buttons.white_left.pin);
+	pin_mask |= BIT(buttons.white_right.pin);
+	pin_mask |= BIT(buttons.yellow.pin);
+	pin_mask |= BIT(buttons.black.pin);
+	pin_mask |= BIT(buttons.green.pin);
+	pin_mask |= BIT(buttons.blue.pin);
+
+	gpio_pin_interrupt_configure_dt(&buttons.white_left, GPIO_INT_EDGE_FALLING);
+	gpio_pin_interrupt_configure_dt(&buttons.white_right, GPIO_INT_EDGE_FALLING);
+	gpio_pin_interrupt_configure_dt(&buttons.yellow, GPIO_INT_EDGE_FALLING);
+	gpio_pin_interrupt_configure_dt(&buttons.black, GPIO_INT_EDGE_FALLING);
+	gpio_pin_interrupt_configure_dt(&buttons.green, GPIO_INT_EDGE_FALLING);
+	gpio_pin_interrupt_configure_dt(&buttons.blue, GPIO_INT_EDGE_FALLING);
+
+	gpio_init_callback(&buttons_func_cb_data, btn_func_pressed, pin_mask);
+	gpio_add_callback(buttons.white_left.port, &buttons_func_cb_data);
+	LOG_INF("Set up callback for function buttons");
+}
+
+static void btn_ctrl_pressed(const struct device *dev, struct gpio_callback *cb, uint32_t pins)
 {
 	if (pins & BIT(buttons.forward.pin))
 	{
 		int val = gpio_pin_get(dev, buttons.forward.pin);
 		LOG_DBG("Forward button pressed, val= %d", val);
-		queue_send_direction(DIR_FORWARD);
+		leds_update(LED_FORWARD_BIT);
 	}
 	if (pins & BIT(buttons.backward.pin))
 	{
 		int val = gpio_pin_get(dev, buttons.backward.pin);
 		LOG_DBG("Backward button pressed, val= %d", val);
-		queue_send_direction(DIR_BACKWARD);
+		leds_update(LED_BACKWARD_BIT);
 	}
 	if (pins & BIT(buttons.stop.pin))
 	{
 		int val = gpio_pin_get(dev, buttons.stop.pin);
 		LOG_DBG("STOP button pressed, val= %d", val);
-		btn_set_stop();
+		leds_update(LED_STOP_BIT);
 	}
+}
+
+void btn_func_handle(const char* name, const struct device *dev, uint32_t pins, gpio_pin_t pin, int led_bits) {
+	if (pins & BIT(pin))
+	{
+		int val = gpio_pin_get(dev, pin);
+		LOG_DBG("%s button pressed, val= %d", name, val);
+		leds_set_rgb(led_bits);
+	}
+
+}
+
+static void btn_func_pressed(const struct device *dev, struct gpio_callback *cb, uint32_t pins)
+{
+	btn_func_handle("White left", dev, pins, buttons.white_left.pin, 1);
+	btn_func_handle("White right", dev, pins, buttons.white_right.pin, 2);
+	btn_func_handle("Yellow", dev, pins, buttons.yellow.pin, 3);
+	btn_func_handle("Black", dev, pins, buttons.black.pin, 4);
+	btn_func_handle("Green", dev, pins, buttons.green.pin, 5);
+	btn_func_handle("Blue", dev, pins, buttons.blue.pin, 6);
 }
